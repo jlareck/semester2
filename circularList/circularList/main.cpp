c0fcbaeaaa6d2f013d1f894266a86f417550bc8b
jlareck <medik2507@gmail.com>
2019-02-27 16:31:06 +0200

    Implemented interactive interface

--- circularList/circularList/main.cpp
+++ circularList/circularList/main.cpp
@@ -13,6 +13,15 @@ struct Point{
     int x;
     int y;
     int z;
+    void enterCoord()
+    {
+        cout << "Enter x coord"<<endl;
+        cin>> x;
+        cout << "Enter y coord"<<endl;
+        cin>> y;
+        cout << "Enter z coord"<<endl;
+        cin>> z;
+    }
 };
 
 struct List{
@@ -44,32 +53,38 @@ int countNodes(List* head)
 
 List* pushBeforeIndex(List* list, int index, Point value)
 {
-    List* node = new List, *prevNode;
-    List* p = list->next;
-    prevNode = list;
-    int numberOfNodes = countNodes(list);
-    if (index>=numberOfNodes)
+    if (countNodes(list)==0)
     {
-        index = index%numberOfNodes;
+      list = addToEmpty(value);
     }
-    do{
-        if (p->index == index)
+    else{
+        List* node = new List, *prevNode;
+        List* p = list->next;
+        prevNode = list;
+        int numberOfNodes = countNodes(list);
+        if (index>=numberOfNodes)
         {
-            node->data = value;
-            prevNode->next = node;
-            node->index = index;
-            node->next = p;
-            while(p!=list->next) {
-                p->index++;
-                p=p->next;
-            }
-            return list;
+            index = index%numberOfNodes;
         }
-        prevNode = p;
-        p = p->next;
-        
-        
-    }while(p!=list->next);
+        do{
+            if (p->index == index)
+            {
+                node->data = value;
+                prevNode->next = node;
+                node->index = index;
+                node->next = p;
+                while(p!=list->next) {
+                    p->index++;
+                    p=p->next;
+                }
+                return list;
+            }
+            prevNode = p;
+            p = p->next;
+            
+            
+        }while(p!=list->next);
+    }
     return list;
 }
 void deleteNode(List* list, int index)
@@ -120,21 +135,22 @@ void setValue(int index, List* list)
     cin >> node->data.z;
     
 }
-void traverse(List *last)
+
+void printCircularList(List *list)
 {
     List *p;
-    if (last == NULL)
+    if (list == NULL)
     {
         cout << "List is empty." << endl;
         return;
     }
-    p = last -> next;
+    p = list -> next;
     do
     {
         cout<< p->index<< ":" << p->data.x << " "<< p->data.y<< " "<<p->data.z<<endl;;
         p = p -> next;
         
-    }while(p != last->next);
+    }while(p != list->next);
 }
 
 void linkedList() {
@@ -165,7 +181,7 @@ void linkedList() {
     point.y = 124;
     point.z = 125;
     pushBeforeIndex(list, 8, point);
-    traverse(list);
+    printCircularList(list);
     cout << "fdasf"<<endl;
     setValue(0, list);
     getValue(0, list);
@@ -179,7 +195,112 @@ void linkedList() {
 //    deleteNode(list, 0);
 //    traverse(list);
 }
-
+void interactiveList()
+{
+    int action;
+    bool checkIfCreatedList = false, loopFlag = true;
+    List* list;
+    while(loopFlag){
+        cout << "Actions" << endl;
+        cout << 1 << " - Create empty"<<endl;
+        cout << 2 << " - Add element before key element"<<endl;
+        cout << 3 << " - Delete element by key" <<endl;
+        cout << 4 << " - Get element" << endl;
+        cout << 5 << " - Set element" <<endl;
+        cout << 6 << " - Print all elements" <<endl;
+        cout << "Press any other key to exit"<<endl;
+        cout << endl;
+        cout << "Enter action"<<endl;
+        cin >> action;
+        switch(action){
+            case 1:{
+                if (!checkIfCreatedList)
+                {
+                    
+                    checkIfCreatedList = true;
+                    cout << "The list was created sucsesfully"<<endl;
+                }
+                else
+                    cout << "You have already created circular list"<<endl;
+                break;
+            }
+            case 2:{
+                if (checkIfCreatedList)
+                {
+                    cout << "Enter index before you want to add element"<<endl;
+                    int index;
+                    
+                    cin>>index;
+                    cout << "Enter value"<<endl;
+                    Point value;
+                    value.enterCoord();
+                    cout << value.x << " f "<< value.y<<endl;
+                    list = pushBeforeIndex(list, index, value);
+                }
+                else
+                    cout << "Error! Firstly you need to create a list"<<endl;
+                break;
+            }
+            case 3:{
+                if (!checkIfCreatedList)
+                {
+                    cout << "You didn't create a list, you need to create and add elements to it if you want to use this function"<<endl;
+                }
+                else if (countNodes(list)==0)
+                    cout << "Error! There is no elements to delete. You need to add some elements in list"<<endl;
+                else{
+                    cout << "Enter the index of element that you want to delete" <<endl;
+                    int index;
+                    cin >> index;
+                    deleteNode(list, index);
+                }
+                break;
+            }
+            case 4:{
+                if (!checkIfCreatedList)
+                {
+                    cout << "Error. You need to create list and add elements in it if you want to use this option"<<endl;
+                }
+                else if (checkIfCreatedList&&countNodes(list)==0) {
+                    cout << "Error! There is no elements in list. You need to add minimum one element if you want to use this case"<<endl;
+                }
+                else{
+                    cout << "Enter the index of element that you want to get"<<endl;
+                    int index;
+                    cin>> index;
+                    cout << "Element "<< index<<endl;
+                    getValue(index, list);
+                }
+                break;
+            }
+            case 5:{
+                if (!checkIfCreatedList)
+                {
+                    cout << "Error! You didn't create a list. Create a list and add some elements in it"<<endl;
+                }
+               else if(countNodes(list)==0) {
+                    cout << "Error! There is no elements in list"<<endl;
+                }
+                else{
+                    cout << "Enter the index of element which you want to set value"<<endl;
+                    int index;
+                    cin>> index;
+                    setValue(index, list);
+                }
+                break;
+            }
+            case 6:{
+                cout << "All elements"<<endl;
+                printCircularList(list);
+                break;
+            }
+            default:
+                loopFlag = false;
+                break;
+                
+        }
+    }
+}
 void addToEmptyVector(vector<int>& vec, int value)
 {
     vec.push_back(value);
@@ -302,8 +423,6 @@ void circularArray(){
 }
 int main()
 {
-    circularVector();
-    cout << "-----"<<endl;
-    circularVector();
+    interactiveList();
     return 0;
 }
