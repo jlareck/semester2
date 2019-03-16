7efd66e1b09edbe798de04bbd91638d236422d84
jlareck <medik2507@gmail.com>
2019-03-16 13:48:26 +0200

    Configured gitignore

---
+++ skiplist/skiplist/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  skiplist
+//
+//  Created by Mykola Medynsky on 3/14/19.
+//  Copyright © 2019 Mykola Medynskyi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
