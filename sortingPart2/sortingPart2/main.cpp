aeec40f32a682ae4c667294dbf791545f924cd21
jlareck <medik2507@gmail.com>
2019-05-03 19:14:13 +0300

    Implemented interactive mode for sorting structure by different parameters and showed benchmark info

--- sortingPart2/sortingPart2/main.cpp
+++ sortingPart2/sortingPart2/main.cpp
@@ -15,7 +15,6 @@
 #include <iostream>
 #include <stdio.h>
 #include <algorithm>
-
 using namespace std;
 using namespace placeholders;
 struct UltimeteAttack{
@@ -47,10 +46,80 @@ void printOneMonster(Monster monster)
     cout << "Date of creation the monster: "<<monster.date << endl;
     cout << endl;
 }
+void createAndAddMonster(vector<Monster>& monsters)
+{
+    Monster monster;
+    if (monsters.size()==0) {
+        monster.id = "0";
+    }
+    else
+        monster.id = to_string(monsters.size());
+    cout << "Enter Name"<<endl;
+    cin.ignore();
+    getline(cin,monster.name);
+    cout << "Enter health points"<<endl;
+    cin >> monster.healthPoint;
+    cout << "Enter damage" <<endl;
+    cin >> monster.damage;
+    cout << "Enter chance of ultimate attack in range 0 to 1"<<endl;
+    cin >> monster.chanceOfUltimateAttack;
+    cout << "Choose ultimate attack:\n 1 - increase damage\n 2 - repeat attack\n 3- healself\n 4 - disable enemy" <<endl;
+    int choice;
+    cin >> choice;
+    switch (choice) {
+        case 1:
+            monster.ultimateAttack.ult = "increase damage";
+            break;
+        case 2:
+            monster.ultimateAttack.ult = "repeat attack";
+            break;
+        case 3:
+            monster.ultimateAttack.ult = "healself";
+            break;
+        case 4:
+            monster.ultimateAttack.ult = "disable enemy";
+            break;
+        default:{
+            cout << "There is no option: "<< choice<<endl;
+            monster.ultimateAttack.ult = "increase damage";
+            break;
+        }
+    }
+    if (monster.ultimateAttack.ult == "increase damage")
+        monster.ultimateAttack.key = 1;
+    else if (monster.ultimateAttack.ult == "repeat attack")
+        monster.ultimateAttack.key = 2;
+    else if (monster.ultimateAttack.ult == "healself")
+        monster.ultimateAttack.key = 3;
+    else
+        monster.ultimateAttack.key = 4;
+    time_t t = std::time(0);
+    tm* now = std::localtime(&t);
+    
+    stringstream ss;
+    if (now->tm_mday<10)
+    {
+        if (now->tm_mon<10)
+            ss<<(now->tm_year + 1900) << "-0"<< (now->tm_mon + 1) << "-0" <<  now->tm_mday;
+        else
+            ss<<(now->tm_year + 1900) << '-'<< (now->tm_mon + 1) << "-0" <<  now->tm_mday;
+    }
+    else{
+        if (now->tm_mon<10)
+            ss<<(now->tm_year + 1900) << "-0"<< (now->tm_mon + 1) << '-' <<  now->tm_mday;
+        else
+            ss<<(now->tm_year + 1900) << '-'<< (now->tm_mon + 1) << '-' <<  now->tm_mday;
+    }
+    monster.date = ss.str();
+    ss.str("");
+    char timestamp_buf[10] = "";
+    strftime (timestamp_buf, 10,"%H.%M.%S", localtime(&t));
+    string timestamp = string(timestamp_buf);
+    monster.time = timestamp;
+    monsters.push_back(monster);
+}
 void addRandomMonster(vector<Monster>& monsters)
 {
-
-    
     Monster monster;
     if (monsters.size()==0) {
         monster.id = "0";
@@ -115,6 +184,9 @@ bool compareTwoStructs(Monster a, Monster b, string s, int counter, vector<strin
         if (s=="damage"){
             return a.damage>b.damage;
         }
+        if (s=="healthPoint"){
+            return a.healthPoint>b.healthPoint;
+        }
         if (s=="ultimateAttack"){
             return a.ultimateAttack.ult>b.ultimateAttack.ult;
         }
@@ -139,6 +211,10 @@ bool compareTwoStructs(Monster a, Monster b, string s, int counter, vector<strin
         if (s=="damage"){
             return a.damage == b.damage ? compareTwoStructs(a, b, s1[counter-1], counter, s1) : a.damage > b.damage;
         }
+        if (s=="healthPoint")
+        {
+            return a.healthPoint == b.healthPoint ? compareTwoStructs(a, b, s1[counter-1], counter, s1) : a.healthPoint > b.healthPoint;
+        }
         if (s=="ultimateAttack"){
             return a.ultimateAttack.ult == b.ultimateAttack.ult ? compareTwoStructs(a, b, s1[counter-1], counter, s1) : a.ultimateAttack.ult > b.ultimateAttack.ult;
         }
@@ -159,122 +235,117 @@ bool compareTwoStructs(Monster a, Monster b, string s, int counter, vector<strin
 }
 
 
-void task() {
+void benchmarkSortingByDifferentParameters() {
     vector<Monster> monsters;
-    for (int i = 0; i < 10; i++)
+    for (int i = 0; i < 100; i++)
     {
         addRandomMonster(monsters);
     }
     
-//    monsters[1].id = "0";
-    for (int i = 0; i < 7; i++)
-    {
-        monsters[i].id = "0";
-    }
-    for (int i =0; i < monsters.size(); i++)
+    for (int i = 0; i < monsters.size(); i++)
     {
         printOneMonster(monsters[i]);
     }
     vector<string> str;
     
     str.push_back("name");
-     str.push_back("id");
+    str.push_back("healthPoints");
     str.push_back("damage");
    
     
     cout << "----------"<<endl;
-    int flag = 1;
-    int a = monsters.size();
-    sort(monsters.begin(), monsters.end(), bind(compareTwoStructs, _1, _2, str[0],flag,str));
+    int counter = 1;
+    clock_t start = clock();
+    sort(monsters.begin(), monsters.end(), bind(compareTwoStructs, _1, _2, str[0],counter,str));
+    clock_t end = clock();
+    cout<< "Time: " << ((double)((end-start))/CLOCKS_PER_SEC) << " seconds"<<endl;
     for (int i =0; i < monsters.size(); i++)
     {
         printOneMonster(monsters[i]);
     }
 }
-void countingSort(vector<Monster>&arr) {
+void countingSort(vector<Monster>&monsters) {
     int min = 1;
     int max = 4;
     int range = max - min + 1;
     
     vector<int> count(range);
-    vector<Monster> output(arr.size());
-    for(int i = 0; i < arr.size(); i++)
-        count[arr[i].ultimateAttack.key-min]++;
+    vector<Monster> output(monsters.size());
+    for(int i = 0; i < monsters.size(); i++)
+        count[monsters[i].ultimateAttack.key-min]++;
     
     for(int i = 1; i < count.size(); i++)
         count[i] += count[i-1];
     
-    for(int i = arr.size()-1; i >= 0; i--)
+    for(int i = monsters.size()-1; i >= 0; i--)
     {
-        output[count[arr[i].ultimateAttack.key-min] -1 ] = arr[i];
-        count[arr[i].ultimateAttack.key-min]--;
+        output[count[monsters[i].ultimateAttack.key-min] -1 ] = monsters[i];
+        count[monsters[i].ultimateAttack.key-min]--;
     }
     
-    for(int i=0; i < arr.size(); i++)
-        arr[i] = output[i];
+    for(int i=0; i < monsters.size(); i++)
+        monsters[i] = output[i];
 }
-void task2(){
+void benchmarkCountingSort(){
     vector<Monster> monsters;
-    for (int i = 0; i < 10; i++)
+    for (int i = 0; i < 10000; i++)
     {
         addRandomMonster(monsters);
     }
-    
-    //    monsters[1].id = "0";
 
     for (int i = 0; i < monsters.size(); i++)
     {
         printOneMonster(monsters[i]);
     }
     cout << "-----------"<<endl;
+     clock_t start = clock();
     countingSort(monsters);
+     clock_t end = clock();
+     cout<< "Time: " << ((double)((end-start))/CLOCKS_PER_SEC) << " seconds"<<endl;
     for (int i =0; i < monsters.size(); i++)
     {
         printOneMonster(monsters[i]);
     }
 }
-int getMax(vector<Monster> arr, int n)
+int getMax(vector<Monster> monsters)
 {
-    int mx = arr[0].healthPoint;
-    for (int i = 1; i < n; i++)
-        if (arr[i].healthPoint > mx)
-            mx = arr[i].healthPoint;
-    return mx;
+    int max = monsters[0].healthPoint;
+    for (int i = 1; i < monsters.size(); i++)
+        if (monsters[i].healthPoint > max)
+            max = monsters[i].healthPoint;
+    return max;
 }
-void countSort(vector<Monster>&arr, int n, int exp)
+void countSort(vector<Monster>&monsters, int exp)
 {
-    vector<Monster> output(n);
+    vector<Monster> output(monsters.size());
     int i, count[10] = {0};
-    for (i = 0; i < n; i++)
-        count[ (arr[i].healthPoint/exp)%10 ]++;
+    for (i = 0; i < monsters.size(); i++)
+        count[ (monsters[i].healthPoint/exp)%10 ]++;
 
     for (i = 1; i < 10; i++)
         count[i] += count[i - 1];
 
-    for (i = n - 1; i >= 0; i--)
+    for (i = monsters.size() - 1; i >= 0; i--)
     {
-        output[count[ (arr[i].healthPoint/exp)%10 ] - 1] = arr[i];
-        count[ (arr[i].healthPoint/exp)%10 ]--;
+        output[count[ (monsters[i].healthPoint/exp)%10 ] - 1] = monsters[i];
+        count[ (monsters[i].healthPoint/exp)%10 ]--;
     }
     
 
-    for (i = 0; i < n; i++)
-        arr[i] = output[i];
+    for (i = 0; i < monsters.size(); i++)
+        monsters[i] = output[i];
 }
 
-void radixsort(vector<Monster>& arr)
+void radixsort(vector<Monster>& monsters)
 {
-
-    int m = getMax(arr, arr.size());
-    
-
-    for (int exp = 1; m/exp > 0; exp *= 10)
-        countSort(arr, arr.size(), exp);
+    int max = getMax(monsters);
+    for (int i = 1; max/i > 0; i *= 10)
+        countSort(monsters, i);
 }
 
 void task3() {
     vector<Monster> monsters;
-    for (int i = 0; i < 10; i++)
+    for (int i = 0; i < 10000; i++)
     {
         addRandomMonster(monsters);
     }
@@ -283,15 +354,142 @@ void task3() {
         printOneMonster(monsters[i]);
     }
     cout << "-----------"<<endl;
+    clock_t start = clock();
     radixsort(monsters);
+    clock_t end = clock();
+    cout<< "Time: " << ((double)((end-start))/CLOCKS_PER_SEC) << " seconds"<<endl;
     for (int i =0; i < monsters.size(); i++)
     {
         printOneMonster(monsters[i]);
     }
 }
-
+void printAll(vector<Monster>monster)
+{
+    for (int i = 0; i< monster.size(); i++)
+    {
+        cout << "ID: " << monster[i].id<<endl;
+        cout << "Name: " << monster[i].name << endl;
+        cout << "Healthpoints: " << monster[i].healthPoint << endl;
+        cout << "Damage: " << monster[i].damage << endl;
+        cout << "Chance of ultimate attack: " << monster[i].chanceOfUltimateAttack << endl;
+        cout << "Ultimate attack: " << monster[i].ultimateAttack.ult << endl;
+        cout << "Time of creation the monster: "<<monster[i].time << endl;
+        cout << "Date of creation the monster: "<<monster[i].date << endl;
+        cout << endl;
+    }
+}
+void interactiveInterface()
+{
+    vector<Monster> monster;
+    bool flag = true;
+    
+    int action;
+    while (flag)
+    {
+        cout << 1 << " - Create new monster" << endl;
+        cout << 2 << " - Print all monsters" << endl;
+        cout << 3 << " - Create random monster" << endl;
+        cout << 4 << " - Sort by different parameters" << endl;
+        cout << 5 << " - Sort using counting sort by ultimate attack" << endl;
+        cout << 6 << " - Sort using radix sort by health points" << endl;
+        cout << 7 << " - Create 10 random monsters"<<endl;
+        cout << 8 << " - Exit" << endl;
+        cout << "Enter action" << endl;
+        cin >> action;
+        switch (action) {
+            case 1:
+                addRandomMonster(monster);
+                break;
+            case 2:
+                printAll(monster);
+                break;
+           
+            case 3:{
+                addRandomMonster(monster);
+                break;
+            }
+            case 4:{
+                bool sortingFlag = true;
+                int sortingChoice;
+                vector<string> sortingParameters;
+                while(sortingFlag)
+                {
+                    cout << "Choose parameters (1-8)" <<endl;
+                    cout << "1 - Sort by ID"<<endl;
+                    cout << "2 - Sort by name"<<endl;
+                    cout << "3 - Sort by healthpoints"<<endl;
+                    cout << "4 - Sort by damage" <<endl;
+                    cout << "5 - Sort by ultimate attack"<<endl;
+                    cout << "6 - Sort by chance of ultimate attack"<<endl;
+                    cout << "7 - Sort by date"<<endl;
+                    cout << "8 - Sort by time"<<endl;
+                    cout << "9 - Sort"<<endl;
+                    cout << "Choose: ";
+                    cin>>sortingChoice;
+                    switch (sortingChoice) {
+                        case 1:
+                            sortingParameters.push_back("id");
+                            break;
+                        case 2:
+                            sortingParameters.push_back("name");
+                            break;
+                        case 3:
+                            sortingParameters.push_back("healthPoint");
+                            break;
+                        case 4:
+                            sortingParameters.push_back("damage");
+                            break;
+                        case 5:
+                            sortingParameters.push_back("ultimateAttack");
+                            break;
+                        case 6:
+                            sortingParameters.push_back("chanceOfUltimateAttack");
+                            break;
+                        case 7:
+                            sortingParameters.push_back("date");
+                            break;
+                        case 8:
+                            sortingParameters.push_back("time");
+                            break;
+                        case 9: {
+                            int count = 1;
+                            sort(monster.begin(), monster.end(), bind(compareTwoStructs, _1, _2, sortingParameters[0],count,sortingParameters));
+                            sortingFlag = false;
+                            break;
+                        }
+                        default:
+                            cout <<"Error! Try again"<<endl;
+                            break;
+                    }
+                }
+                break;
+            }
+            case 5:
+                countingSort(monster);
+                break;
+            case 6:
+                radixsort(monster);
+                break;
+            case 7:{
+                for (int i = 0; i < 10; i++)
+                    addRandomMonster(monster);
+                break;
+            }
+            case 8:{
+                cout << "Good bye!"<<endl;
+                flag = false;
+                break;
+            }
+            default:
+                cout << "Error! Try again"<<endl;
+                break;
+        }
+    }
+}
 int main(int argc, const char * argv[]) {
     srand(time(nullptr));
-    task3();
+  //  interactiveInterface();
+    benchmarkSortingByDifferentParameters();
     return 0;
 }
+
