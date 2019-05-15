7534c72196b47c76ca8200d9a7f7b8d669415bc8
jlareck <medik2507@gmail.com>
2019-05-15 16:20:49 +0300

    Implemented interactive modes

--- tree/tree/main.cpp
+++ tree/tree/main.cpp
@@ -30,50 +30,37 @@ Node* createNode(int key, int data) {
     return newNode;
 }
 bool hasPath(vector<int>& pathArr, Node* root, int searchKey) {
-    // if root is NULL
-    // there is no path
+
     if (!root)
-    return false;
-    
-    // push the node's value in 'arr'
+        return false;
+
     pathArr.push_back(root->key);
     
-    // if it is the required node
-    // return true
+
     if (root->key == searchKey)
-    return true;
-    
-    // else check whether the required node lies
-    // in the left subtree or right subtree of
-    // the current node
-   // int i = 0;
+        return true;
+
     for (int i = 0; i < root->child.size();i++)
     {
         if (hasPath(pathArr, root->child[i], searchKey))
             return true;
     }
-    // required node does not lie either in the
-    // left or right subtree of the current node
-    // Thus, remove current node's value from
-    // 'arr'and then return false
+
     pathArr.pop_back();
     return false;
 }
 void printPath(Node *root, int x)
 {
-    // vector to store the path
+
     vector<int> arr;
-    
-    // if required node 'x' is present
-    // then print the path
+
     if (hasPath(arr, root, x))
     {
         for (int i=0; i<arr.size()-1; i++)
-        cout << arr[i] << "->";
+            cout << arr[i] << "->";
         cout << arr[arr.size() - 1];
     }
-    
-    // 'x' is not present in the binary tree
+
     else
         cout << "No Path";
 }
@@ -113,29 +100,25 @@ Node* addElementbyPath(Node* tmpRoot, vector<int> arr, int& j, bool& flag, int d
     }
     return tmpRoot;
 }
-Node* deleteElementbyPath(Node* &tmpRoot, vector<int> arr, int& j, bool& flag, Node*& deletedSubTree)
+void deleteElementbyPath(Node* &tmpRoot, vector<int> arr, int& j, bool& flag, Node*& deletedSubTree)
 {
     
     if (tmpRoot->key == arr.back()){
         deletedSubTree = tmpRoot;
         tmpRoot = nullptr;
         flag = true;
-        return deletedSubTree;
-        
     }
     else {
         for (int i = 0; i < tmpRoot->child.size(); i++)
         {
             if (tmpRoot->child[i]->key==arr[j])
             {
-        
+                
                 j++;
                 deleteElementbyPath(tmpRoot->child[i], arr, j, flag, deletedSubTree);
             }
         }
-        return tmpRoot;
     }
-    
 }
 BinaryTreeNode* newNode(int item)
 {
@@ -148,73 +131,60 @@ BinaryTreeNode* insert(BinaryTreeNode* node, int key)
 {
     if (node == NULL) return newNode(key);
     if (key < node->data)
-    node->left  = insert(node->left, key);
+        node->left  = insert(node->left, key);
     else if (key >=  node->data)
-    node->right = insert(node->right, key);
+        node->right = insert(node->right, key);
     return node;
 }
 BinaryTreeNode *createThreaded(BinaryTreeNode *root)
 {
-    // Base cases : Tree is empty or has single
-    //              node
+
     if (root == NULL)
-    return NULL;
+        return NULL;
     if (root->left == NULL &&
         root->right == NULL)
-    return root;
-    
-    // Find predecessor if it exists
+        return root;
     if (root->left != NULL)
     {
-        // Find predecessor of root (Rightmost
-        // child in left subtree)
         BinaryTreeNode* l = createThreaded(root->left);
-        
-        // Link a thread from predecessor to
-        // root.
         l->right = root;
         l->isThreaded = true;
     }
     
-    // If current node is rightmost child
+
     if (root->right == NULL)
-    return root;
+        return root;
     
-    // Recur for right subtree.
+
     return createThreaded(root->right);
 }
 BinaryTreeNode *leftMost( BinaryTreeNode *root)
 {
     while (root != NULL && root->left != NULL)
-    root = root->left;
+        root = root->left;
     return root;
 }
-
-// Function to do inorder traversal of a threadded
-// binary tree
 void inOrder(BinaryTreeNode *root)
 {
     if (root == NULL) return;
     
-    // Find the leftmost node in Binary Tree
+
     BinaryTreeNode *cur = leftMost(root);
     
     while (cur != NULL)
     {
         cout << cur->data << " ";
         
-        // If this Node is a thread Node, then go to
-        // inorder successor
         if (cur->isThreaded)
             cur = cur->right;
         
-        else // Else go to the leftmost child in right subtree
+        else
             cur = leftMost(cur->right);
     }
 }
 void printTreeInBrackets(BinaryTreeNode* node) {
     if (!node) {
-        //cout<<".";
+ 
         return;
     }
     cout<<node->data<<"(";
@@ -226,7 +196,6 @@ void printTreeInBrackets(BinaryTreeNode* node) {
 }
 void printTreeInBrackets(Node* node) {
     if (!node) {
-        //cout<<".";
         return;
     }
     cout<<node->data<<"(";
@@ -254,73 +223,227 @@ void task1() {
     root->child.push_back(createNode(1, 1));
     root->child.push_back(createNode(2, 2));
     root->child[0]->child.push_back(createNode(3, 3));
-     root->child[0]->child.push_back(createNode(4, 4));
-     root->child[0]->child.push_back(createNode(5, 5));
-     root->child[0]->child[2]->child.push_back(createNode(6, 6));
+    root->child[0]->child.push_back(createNode(4, 4));
+    root->child[0]->child.push_back(createNode(5, 5));
+    root->child[0]->child[2]->child.push_back(createNode(6, 6));
     
     cout << root->child[0]->child[0]->data<<endl;
- //   printPath(root, 6);
+
     vector<int> arr;
     hasPath(arr, root, 6);
- //   arr.push_back(3);
-    
+
     int i = 1;
     bool flag = false;
-  root = addElementbyPath(root, arr, i, flag,7,7);
+    root = addElementbyPath(root, arr, i, flag,7,7);
     Node* prevNode = new Node;
     i = 1;
-   // cout <<root->child[0]->child[2]->child[0]->child[0]->data<<endl;
-  deleteElementbyPath(root, arr, i, flag, prevNode);
+
+    deleteElementbyPath(root, arr, i, flag, prevNode);
     
     printTreeInBrackets(root);
-//    cout <<arr.back()<<endl;
- //   getElementbyPath(root, arr, i, flag);
+
     if (!flag)
         cout << "error"<<endl;
-
+    
 }
 void interactiveMode() {
     int action;
     bool flag = true;
     while(flag){
-        cout << "1 - Interactive mode n-ary tree"<<endl;
-        cout << "2 - Interactive mode binary tree"<<endl;
-        cin>>action;
-        switch (action) {
+        cout << "1 - Interactive mode n-ary tree" << endl;
+        cout << "2 - Interactive mode binary tree" << endl;
+        cout << "3 - Exit"<<endl;
+        cin >> action;
+        switch (action)
+        {
             case 1:{
                 Node* root = NULL;
                 vector<int> keys;
                 int actionNaryTree;
                 bool flagNaryTree = true;
                 while (flagNaryTree) {
-                    cout << "1 - Add element to tree by path"<<endl;
-                    cout << "2 - Get element by path"<<endl;
-                    cout << "3 - Get path to element"<<endl;
-                    cout << "4 - Delete element by path"<<endl;
-                    cout << "5 - Print tree"<<endl;
+                    cout << "1 - Add element to tree by path" << endl;
+                    cout << "2 - Get element by path" << endl;
+                    cout << "3 - Get path to element" << endl;
+                    cout << "4 - Delete element by path" << endl;
+                    cout << "5 - Print tree" << endl;
+                    cout << "6 - Exit" << endl;
                     cin >> actionNaryTree;
                     switch (actionNaryTree) {
-                        case 1:
-                        
-                        break;
-                        
+                        case 1: {
+                            int key;
+                            int data;
+                            vector<int> path;
+                            int sizeOfPath;
+                            int keyInPath;
+                            if (keys.size()==0) {
+                                cout << "Enter key"<<endl;
+                                cin >> key;
+                                
+                                keys.push_back(key);
+                                cout << "Enter data" << endl;
+                                cin >> data;
+                                root = createNode(key, data);
+                            }
+                            else {
+                                int j=1;
+                                bool addElementFlag = false;
+                                cout << "Enter size of path" << endl;
+                                cin>>sizeOfPath;
+                                cout << "Enter path" << endl;
+                                while(path.size()!=sizeOfPath)
+                                {
+                                    cin >> keyInPath;
+                                    path.push_back(keyInPath);
+                                }
+                                cout << "Enter key" << endl;
+                                cin >> key;
+                                while(find(keys.begin(), keys.end(), key)!=keys.end()){
+                                    cout << "Enter another key, because this key already exists"<<endl;
+                                    cin >> key;
+                                }
+                                cout << "Enter data" << endl;
+                                cin >> data;
+                                root = addElementbyPath(root, path, j, addElementFlag, data, key);
+                                if (!addElementFlag)
+                                    cout << "Error! You choose wrong path" << endl;
+                            }
+                            break;
+                        }
+                            
+                        case 2: {
+                            vector<int> path;
+                            int sizeOfPath;
+                            int keyInPath;
+                            bool getElementFlag = false;
+                            cout << "Enter size of path" << endl;
+                            cin>>sizeOfPath;
+                            cout << "Enter path" << endl;
+                            while(path.size()!=sizeOfPath)
+                            {
+                                cin >> keyInPath;
+                                path.push_back(keyInPath);
+                            }
+                            if (path[0]==root->key){
+                                int j = 1;
+                                getElementbyPath(root, path, j, getElementFlag);
+                            }
+                            if (!getElementFlag)
+                                cout << "Error! Wrong path!" << endl;
+                            break;
+                        }
+                        case 3: {
+                 
+                            int key;
+                            cout << "Enter the key to what you want to have the path" << endl;
+                            cin >> key;
+                            printPath(root, key);
+                            break;
+                        }
+                        case 4:{
+                            int j=1;
+                            int sizeOfPath;
+                            vector<int> path;
+                            int keyInPath;
+                            bool deleteElementFlag = false;
+                            Node* deletedSubTree = nullptr;
+                            cout << "Enter size of path" << endl;
+                            cin>>sizeOfPath;
+                            cout << "Enter path" << endl;
+                            while(path.size()!=sizeOfPath)
+                            {
+                                cin>>keyInPath;
+                                path.push_back(keyInPath);
+                            }
+                            if(path[0]==root->key&&path.size()<=keys.size()){
+                                
+                                deleteElementbyPath(root, path, j, deleteElementFlag, deletedSubTree);
+                            }
+                            if (!deleteElementFlag) {
+                                cout << "You entered wrong path" << endl;
+                            }
+                            else{
+                                int actionPrint;
+                                cout << "If you want to print deleted subtree press 1" << endl;
+                                cin >> actionPrint;
+                                if (actionPrint == 1)
+                                    printTreeInBrackets(deletedSubTree);
+                            }
+                            break;
+                        }
+                        case 5:{
+                            cout << "Tree:"<<endl;
+                            printTreeInBrackets(root);
+                            break;
+                        }
+                        case 6: {
+                            cout << "Good bye!"<<endl;
+                            flagNaryTree = false;
+                            break;
+                        }
                         default:
-                        break;
+                            cout << "There is no action: "<< actionNaryTree << endl;
+                            break;
                     }
                 }
                 
-                
-                
             }
-            break;
-            
+            case 2: {
+                BinaryTreeNode* root = nullptr;
+                vector<int> keys;
+                int actionNaryTree;
+                bool flagBinaryTree = true;
+                while (flagBinaryTree) {
+                    cout << "1 - Add element to binary tree" << endl;
+                    cout << "2 - Build threaded tree" << endl;
+                    cout << "3 - Print tree" << endl;
+                    cout << "4 - Exit" << endl;
+                    cin >> actionNaryTree;
+                    switch (actionNaryTree) {
+                        case 1:
+                            int data;
+                            cout<< "Enter data"<<endl;
+                            cin >> data;
+                           root = insert(root, data);
+                            break;
+                        case 2:{
+                            createThreaded(root);
+                            int printAction;
+                            cout << "Do you want to print it inorder"<<endl;
+                            cout << "1 - Yes, any other key - No"<<endl;
+                            cin>> printAction;
+                            if(printAction)
+                                inOrder(root);
+                            break;
+                        }
+                        case 3:{
+                            cout << "Tree:"<<endl;
+                            printTreeInBrackets(root);
+                            break;
+                        }
+                        case 4:{
+                            cout << "Good bye!"<<endl;
+                            flagBinaryTree = false;
+                            break;
+                        }
+                        default:
+                            cout << "Error! try another action" << endl;
+                            break;
+                    }
+                }
+                break;
+            }
+            case 3:
+                cout << "Good bye"<<endl;
+                flag = false;
+                break;
             default:
-            break;
+                break;
         }
     }
 }
 int main()
 {
-    task2();
+    interactiveMode();
     return 0;
 }
